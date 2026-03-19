ea051d209ada54e2201bccf3994cd95023ff28d5
vishalkakiyan <vishalkakiyan@gmail.com>
2026-03-19 20:19:34 +0530

    feat: Add Vercel Analytics for enhanced performance tracking and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body>
         <TooltipProvider>{children}</TooltipProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
